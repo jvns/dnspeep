25e53a98cc87443504ccbf957cb66b68424d23fd
Julia Evans <julia@jvns.ca>
2021-04-02 09:55:19 -0400

    Add all DNS record types that dns-message-parser supports

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ use etherparse::PacketHeaders;
 use eyre::{Result, WrapErr};
 use futures::StreamExt;
 use getopts::Options;
+use hex::encode;
 use pcap::stream::{PacketCodec, PacketStream};
 use pcap::{Active, Capture, Linktype, Packet};
 use std::collections::HashMap;
 use std::env;
 use std::net::IpAddr;
 use std::str;
+use std::str::from_utf8;
 use std::sync::{Arc, Mutex};
 use tokio::time::{delay_for, Duration};
 
@@ -271,15 +273,94 @@ fn format_record(rdata: &RR) -> String {
     match rdata {
         RR::A(x) => format!("A: {}", x.ipv4_addr),
         RR::AAAA(x) => format!("AAAA: {}", x.ipv6_addr),
+        RR::AFSDB(x) => format!("AFSDB: {} {}", x.subtype, x.hostname),
+        RR::APL(x) => {
+            // not in use
+            let formatted: Vec<String> = x.apitems.iter().map(|x| x.to_string()).collect();
+            format!("APL: {}", formatted.join(" "))
+        }
+        RR::CAA(x) => format!(
+            "CAA: {} {} {}",
+            x.flags,
+            x.tag,
+            from_utf8(&x.value).unwrap() // TODO: do better error handling here than an unwrap()
+        ),
         RR::CNAME(x) => format!("CNAME: {}", x.c_name),
-        RR::PTR(x) => format!("PTR: {}", x.ptr_d_name),
+        RR::DNAME(x) => format!("DNAME: {}", x.target),
+        RR::DNSKEY(x) => format!(
+            "DNSKEY: 3 {} {} {}",
+            x.get_flags(),
+            x.algorithm_type.clone() as u8,
+            encode(&x.public_key),
+        ),
+        RR::DS(x) => format!(
+            "DS: {} {} {} {}",
+            x.key_tag,
+            x.algorithm_type,
+            x.digest_type,
+            encode(&x.digest),
+        ),
+        RR::EID(x) => format!("EID: {}", from_utf8(&x.data).unwrap()), // not in use
+        RR::EUI48(x) => format!(
+            "EUI48: {:02x}-{:02x}-{:02x}-{:02x}-{:02x}-{:02x}",
+            x.eui_48[0], x.eui_48[1], x.eui_48[2], x.eui_48[3], x.eui_48[4], x.eui_48[5],
+        ),
+        RR::EUI64(x) => format!(
+            "EUI64: {:02x}-{:02x}-{:02x}-{:02x}-{:02x}-{:02x}-{:02x}-{:02x}",
+            x.eui_64[0],
+            x.eui_64[1],
+            x.eui_64[2],
+            x.eui_64[3],
+            x.eui_64[4],
+            x.eui_64[5],
+            x.eui_64[6],
+            x.eui_64[7],
+        ),
+        RR::GPOS(x) => format!("GPOS: {} {} {}", x.latitude, x.longitude, x.altitude),
+        RR::HINFO(x) => format!("HINFO: {} {}", x.cpu, x.os),
+        RR::ISDN(x) => format!("ISDN: {}", x.isdn_address),
+        RR::KX(x) => format!("KX: {} {}", x.preference, x.exchanger),
+        RR::L32(x) => {
+            let bytes = x.locator_32.to_be_bytes();
+            format!(
+                "L32: {} {} {} {} {}",
+                x.preference, bytes[0], bytes[1], bytes[2], bytes[3]
+            )
+        }
+        RR::L64(x) => format!("L64: {} {}", x.preference, x.locator_64),
+        RR::LOC(x) => format!(
+            "LOC: {} {} {} {} {} {}",
+            x.size, x.horiz_pre, x.vert_pre, x.latitube, x.longitube, x.altitube
+        ),
+        RR::LP(x) => format!("LP: {} {}", x.preference, x.fqdn),
+        RR::MB(x) => format!("MB: {}", x.mad_name),
+        RR::MD(x) => format!("MD: {}", x.mad_name),
+        RR::MF(x) => format!("MF: {}", x.mad_name),
+        RR::MG(x) => format!("MG: {}", x.mgm_name),
+        RR::MINFO(x) => format!("MINFO: {} {}", x.r_mail_bx, x.e_mail_bx),
+        RR::MR(x) => format!("MR: {}", x.new_name),
         RR::MX(x) => format!("MX: {} {}", x.preference, x.exchange),
+        RR::NID(x) => format!("NID: {} {}", x.preference, x.node_id),
+        RR::NIMLOC(x) => format!("NIMLOC: {}", from_utf8(&x.data).unwrap()), // not in use
         RR::NS(x) => format!("NS: {}", x.ns_d_name),
+        RR::NSAP(x) => format!("NSAP: {}", from_utf8(&x.data).unwrap()), // not in use
+        RR::NULL(x) => format!("NULL: {}", from_utf8(&x.data).unwrap()),
+        RR::OPT(_) => panic!("didn't expect to see an OPT record in the answer section"),
+        RR::PTR(x) => format!("PTR: {}", x.ptr_d_name),
+        RR::PX(x) => format!("PX: {} {} {}", x.preference, x.map822, x.mapx400), // not in use
+        RR::RP(x) => format!("RP: {} {}", x.mbox_dname, x.txt_dname),
+        RR::RT(x) => format!("RT: {} {}", x.preference, x.intermediate_host),
         RR::SOA(x) => format!("SOA:{}...", x.m_name),
         RR::SRV(x) => format!("SRV: {} {} {} {}", x.priority, x.weight, x.port, x.target),
-        RR::URI(x) => format!("URI: {} {} {}", x.priority, x.weight, x.uri),
-        RR::HINFO(x) => format!("URI: {} {}", x.cpu, x.os),
+        RR::SSHFP(x) => format!(
+            "SSHFP: {} {} {}",
+            x.algorithm,
+            x.type_,
+            encode(x.fp.as_slice())
+        ),
         RR::TXT(x) => format!("TXT: {}", x.string),
-        _ => panic!("I don't recognize that query type, {:?}", rdata),
+        RR::URI(x) => format!("URI: {} {} {}", x.priority, x.weight, x.uri),
+        RR::WKS(x) => format!("WKS: {} {:x?}", x.protocol, x.bit_map),
+        RR::X25(x) => format!("X25: {}", x.psdn_address), // not in use
     }
 }
